c6fb609ac7c4ec4bb8fc50f49195693f85748e72
Svintsov Dmitry <root@uralbash.ru>
2017-06-30 16:22:52 +0500

    ++sitemap

--- docs/config_sphinx.py
+++ docs/config_sphinx.py
@@ -10,6 +10,7 @@
 import os
 import sys
 import subprocess
+import xml.etree.ElementTree as ET
 from datetime import datetime
 
 # Docs
@@ -223,6 +224,39 @@ def sourcecode(role, rawtext, text, lineno, inliner,
     return [node], []
 
 
+def add_html_link(app, pagename, templatename, context, doctree):
+    """As each page is built, collect page names for the sitemap"""
+    _prefix = 'LECTURES_BASE_URL'
+    base_url = os.environ.get(_prefix)
+    if base_url:
+        app.sitemap_links.append(base_url + pagename + ".html")
+
+
+def create_sitemap(app, exception):
+    """Generates the sitemap.xml from the collected HTML page links"""
+    if (exception is not None or not app.sitemap_links):
+        return
+
+    filename = app.outdir + "/sitemap.xml"
+    print("Generating sitemap.xml in %s" % filename)
+
+    root = ET.Element("urlset")
+    root.set("xmlns", "http://www.sitemaps.org/schemas/sitemap/0.9")
+
+    for link in app.sitemap_links:
+        url = ET.SubElement(root, "url")
+        ET.SubElement(url, "loc").text = link
+
+    ET.ElementTree(root).write(filename)
+
+
+def set_base_url(url):
+    _prefix = 'LECTURES_BASE_URL'
+    if os.environ.get(_prefix):
+        return
+    os.environ[_prefix] = url
+
+
 def setup(app):
     """Install the plugin.
 
@@ -238,3 +272,14 @@ def setup(app):
 
     # Add roles
     app.add_role('src', sourcecode)
+
+    # sitemap
+    """Setup conntects events to the sitemap builder"""
+    app.connect('html-page-context', add_html_link)
+    app.connect('build-finished', create_sitemap)
+    _prefix = 'LECTURES_BASE_URL'
+    base_url = os.environ.get(_prefix)
+    app.sitemap_links = [
+        "{}index.html".format(base_url),
+        "{}genindex.html".format(base_url),
+    ]

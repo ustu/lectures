850fea0f77bb73d04799b0ce225963af590bfc87
Svintsov Dmitry <root@uralbash.ru>
2017-10-12 14:00:44 +0500

    clean up

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
 
 let
   stdenv = pkgs.stdenv;
-  pythonPackages = pkgs.python36Packages;
+  pythonPackages = pkgs.python3Packages;
 
   sphinx_links = pythonPackages.buildPythonPackage rec {
     name = "sphinx_links";
@@ -17,47 +17,6 @@ let
     doCheck = false; # some files required by the test seem to be missing
   };
 
-  markupSafe = pythonPackages.buildPythonPackage rec {
-    name = "MarkupSafe";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "pallets";
-      repo = "markupsafe";
-      rev = "master";
-      sha256 = "10wfpivc5d28jan4xgzi4j6f3lnyy2bzvi35yv75ml7xs3f0hc6w";
-    };
-
-    doCheck = false; # some files required by the test seem to be missing
-  };
-
-  pyscss = pythonPackages.buildPythonPackage rec {
-    name = "pyscss";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "Kronuz";
-      repo = "pyScss";
-      rev = "master";
-      sha256 = "1scwlw2zpbjgkd2xsnwygj5fx157xv2qddpzb8rj9svrxyj0nqw4";
-    };
-
-    buildInputs = with pythonPackages; [ six ];
-
-    doCheck = false; # some files required by the test seem to be missing
-  };
-
-  mako = pythonPackages.buildPythonPackage rec {
-    name = "mako";
-
-    src = pkgs.fetchurl {
-      url = mirror://pypi/P/Paste/Mako-1.0.4.tar.gz;
-      sha256 = "0nchpw6akfcsg8w6irjlx0gyzadc123hv4g47sijgnqd9nz9vngy";
-    };
-
-    buildInputs = [ markupSafe ];
-
-    doCheck = false; # some files required by the test seem to be missing
-  };
-
   zzzeeksphinx = pythonPackages.buildPythonPackage rec {
     name = "zzzeeksphinx";
 
@@ -68,7 +27,12 @@ let
       sha256 = "1zrzivdq6ndm5s63g6jk301yna87812pzcr3r54d5hfn9ykqci5y";
     };
 
-    buildInputs = [ mako pyscss markupSafe pythonPackages.six ];
+    buildInputs = [
+      pythonPackages.Mako
+      pythonPackages.pyscss
+      pythonPackages.markupsafe
+      pythonPackages.six
+    ];
 
     doCheck = false; # some files required by the test seem to be missing
   };
@@ -92,7 +56,7 @@ in rec {
   pyEnv = stdenv.mkDerivation {
     name = "lectures-1.0.0";
     buildInputs = with pythonPackages; [
-      mako
+      Mako
       pyscss
       sphinx
       ipython

9bbffab9ffca12141f3cf7741c29b57ae59bca1e
tommybolin <tommybolin@19216801.ru>
2018-10-19 15:01:03 +0500

    fix up

--- docs/config_sphinx.py
+++ docs/config_sphinx.py
@@ -34,7 +34,7 @@ process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
 output, error = process.communicate()
 
 # Install
-bashCommand = "pip install -r ../lectures/requirements.txt"
+bashCommand = "pip install -r ../_lectures/requirements.txt"
 process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
 output, error = process.communicate()
 
